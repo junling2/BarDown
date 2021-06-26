f42973d39ddf52abd13992764f53b469c19a43ea
junling2 <junling2@ualberta.ca>
2021-06-26 16:53:10 -0700

    Create React App from static templates
    change static elements in index.html into a dynamic react page where element data are mapped to a javascript array.

---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Mercurial Vapor 14 Elite FG',
+            category: 'Mens Cleats',
+            image: '/images/p1.jpg',
+            price: 330,
+            brand: 'Nike', 
+            rating: 5,
+            numReviews: 1032,
+            description: 'The Nike Mercurial Vapor 14 Elite AG features a new look with specialised components to let you play your fastest from start to finish. A cushioned insole helps prevent your foot from sliding, and the innovative plate provides instant responsiveness for quicker cuts at high speeds.',
+        },
+        {
+            _id: '2',
+            name: 'Nike Phantom GT Elite FG',
+            category: 'Mens Cleats',
+            image: '/images/phantom-gt.jpg',
+            price: 325,
+            brand: 'Nike', 
+            rating: 4.5,
+            numReviews: 456,
+            description: 'The Nike Phantom GT Elite FG is a data-driven design engineered for precise attacks. Grippy texture across the Flyknit top and specialised traction on the bottom let you take control of the game.',
+        },
+        {
+            _id: '3',
+            name: 'Nike Tiempo Legend 8 Elite FG',
+            category: 'Mens Cleats',
+            image: '/images/tiempo-legend.jpg',
+            price: 210,
+            brand: 'Nike', 
+            rating: 4.5,
+            numReviews: 45,
+            description: 'The Nike Tiempo Legend 8 Elite FG takes the legendary touch of premium kangaroo leather and adds foot-hugging Quadfit mesh in the lining and a wraparound Flyknit tongue that feels supportive under your arch.'
+        },
+        {
+            _id: '4',
+            name: 'Nike Hypervenom III Academy FG',
+            category: 'Mens Cleats',
+            image: '/images/hypervenom-III.jpg',
+            price: 92,
+            brand: 'Nike', 
+            rating: 2.5,
+            numReviews: 13,
+            decription: 'The Nike Hypervenom III has a solid leather touch and form-fitting tougue for increased lock down',
+        },
+    ]
+}
+
+export default data;
