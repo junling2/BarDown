4b8a498b3bd8401a3d2c5f174b55dbc633df4ebd
junling2 <junling2@ualberta.ca>
2021-06-26 20:45:48 -0700

    Seperate App.js into Product and Rating files
    modularize ratings system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,28 @@
 import data from './data';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">Bar Down</a>
-            </div>
-            <div>
-                <a href="/myaccount">My account</a>
-                <a href="/cart">Cart</a>
-            </div>
-        </header>
-        <main>
-            <div className="row center">
-              {
-                data.products.map(product => (
-                  <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                        <img className="medium" src={product.image} alt={product.name}/>
-                    </a>
-                    <div className="card-body">
-                        <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                        </div>
-                        <div className="price">${product.price}</div>
-                    </div>
-                </div>
-                ))
-              }
-            </div>
-        </main>
-        <footer className="row center">All Rights Reserved</footer>
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Bar Down</a>
+        </div>
+        <div>
+          <a href="/myaccount">My account</a>
+          <a href="/cart">Cart</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {
+            data.products.map(product => (
+              <Product key={product._id} product={product}></Product>
+            ))
+          }
+        </div>
+      </main>
+      <footer className="row center">All Rights Reserved</footer>
     </div>
   );
 }
