0d613d411880e32a333e29b846800a98912d5f81
junling2 <junling2@ualberta.ca>
2021-06-29 16:33:16 -0700

    Create Node application

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server is ready");
+});
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
