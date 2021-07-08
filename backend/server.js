fdde910f4b4be33a30d6b73da802b9f1f375ed9a
junling2 <junling2@ualberta.ca>
2021-07-08 13:25:56 -0700

    Install MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Server is ready");
-});
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+mongoose.connect('mongodb://localhost/BarDown', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 app.get('/api/products/:id', (req, res) => {
@@ -20,6 +19,18 @@ app.get('/api/products/:id', (req, res) => {
         res.status(404).send({ message: 'Product not Found' });
     }
 });
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+app.use('/api/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.send("Server is ready");
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`);
