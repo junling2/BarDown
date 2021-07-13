1f58e86a3947196a7e7bf61b3a35d6c939934035
junling2 <junling2@ualberta.ca>
2021-07-12 18:47:49 -0700

    Create Signin Screen
    Create and style form for Sign In page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
 
@@ -22,12 +23,13 @@ function App() {
                 <span className='badge'>{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/myaccount">My Account</Link>
+            <Link to="/signin">My Account</Link>
           </div>
         </header>
         <main>
           <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/signin' component={SigninScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All Rights Reserved</footer>
