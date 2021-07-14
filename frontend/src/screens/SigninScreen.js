aeb9e5e3c70dbad53954d1e3f18264696ab06ad9
junling2 <junling2@ualberta.ca>
2021-07-13 21:07:20 -0700

    Implement Signin And Signout Actions
    Define user actions and reducers to change states in redux store for signin  request, success/fail, and signout

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen() {
+export default function SigninScreen(props) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const redirect = props.location.search ? props.location.search.split('=')[1] : '/';
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo, loading, error } = userSignin;
+
+
     const submitHandler = (e) => {
         e.preventDefault();
-        //TODO: signin action
+        dispatch(signin(email, password));
     }
+    useEffect(() => {
+        if (userInfo) {
+            props.history.push(redirect);
+        }
+    }, [props.history, redirect, userInfo])
 
     return (
         <div>
@@ -16,6 +31,8 @@ export default function SigninScreen() {
                 <div>
                     <h1>Sign In</h1>
                 </div>
+                {loading && <LoadingBox></LoadingBox>}
+                {error && <MessageBox variant="error">{error}</MessageBox>}
                 <div>
                     <label htmlFor='email'>Email Address</label>
                     <input type='email' id='email' placeholder='Enter Email' required onChange={e => setEmail(e.target.value)}></input>
