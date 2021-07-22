04b7000a85c5d54f7c2a8626d378c88f3de72bc2
junling2 <junling2@ualberta.ca>
2021-07-22 14:35:15 -0700

    Add PlaceOrder frontend screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,66 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import { signout } from './actions/userActions';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import PaymentMethodScreen from './screens/PaymentMethodScreen';
-import ProductScreen from './screens/ProductScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingAddressScreen from './screens/ShippingAddressScreen';
-import SigninScreen from './screens/SigninScreen';
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import ProductScreen from "./screens/ProductScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
-
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
   const signoutHandler = () => {
     dispatch(signout());
-  }
+  };
 
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <Link className="brand" to="/">Bar Down</Link>
+            <Link className="brand" to="/">
+              Bar Down
+            </Link>
           </div>
           <div>
-            <Link to="/cart">Cart
-            {cartItems.length > 0 && (
-                <span className='badge'>{cartItems.length}</span>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            {
-              userInfo ? (
-                <div className="dropdown">
-                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i> </Link>
-                  <ul className="dropdown-content">
-                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
-                  </ul>
-                </div>
-              ) :
-                <Link to="/signin">My Account</Link>
-            }
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">My Account</Link>
+            )}
           </div>
         </header>
         <main>
-          <Route path='/cart/:id?' component={CartScreen}></Route>
-          <Route path='/product/:id' component={ProductScreen}></Route>
-          <Route path='/signin' component={SigninScreen}></Route>
-          <Route path='/register' component={RegisterScreen}></Route>
-          <Route path='/shipping' component={ShippingAddressScreen}></Route>
-          <Route path='/payment' component={PaymentMethodScreen}></Route>
-          <Route path='/' component={HomeScreen} exact></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All Rights Reserved</footer>
       </div>
