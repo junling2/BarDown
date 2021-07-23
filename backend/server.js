2909d12b5956e9b9adc4667ddaf59964f938a675
junling2 <junling2@ualberta.ca>
2021-07-23 14:35:42 -0700

    Make placeOrder API
    1. define order schema for mongoose in order model
    2. define order API route in order router
    3. assign order router to placeorder url in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,31 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routers/userRouter.js';
-import productRouter from './routers/productRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/BarDown', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/BarDown", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
-app.get('/', (req, res) => {
-    res.send("Server is ready");
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
+  console.log(`Server at http://localhost:${port}`);
 });
--- backend/utils.js
+++ backend/utils.js
@@ -1,14 +1,37 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-    return jwt.sign({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
     },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '30d',
-        });
-}
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somethingsecret",
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "invalid token" });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "no token" });
+  }
+};
